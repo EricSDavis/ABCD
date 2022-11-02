00f1ef7eaedc28ba844caff481d07863f1d099f6
EricSDavis <ericscottdavis@outlook.com>
2022-11-02 07:19:14 -0400

    Update for cluster execution.

--- Makefile
+++ Makefile
@@ -1,26 +1,27 @@
 .PHONY: clean
 
 objects :=\
-	data/greeting.rds\
-	plots/example.pdf\
-	tables/example.txt
+	data/h5/expectedCounts.h5\
+	data/h5/expectedCounts.rds
 
 all: $(objects)
 
 clean:
 	rm -rf $(objects)
 
-data/greeting.rds:\
-	scripts/utils/sayHello.R\
-	scripts/processing/makeGreeting.R
-		mkdir -p data
-		Rscript scripts/processing/makeGreeting.R
-
-plots/example.pdf\
-tables/example.txt:\
-	scripts/utils/sayHello.R\
-	data/greeting.rds\
-	scripts/analysis/example.R
-		mkdir -p plots tables
-		Rscript scripts/analysis/example.R
-		
+## Rule pattern necessary for this version of make
+data/h5/%Counts.h5\
+data/h5/%Counts.rds:\
+	data/raw/LIMA_binnedEPCounts.rda\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0000_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0000_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0030_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0030_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0060_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0060_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0090_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0090_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0120_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0120_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0240_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0240_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0360_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0360_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_1440_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_1440_S_0.0.0_megaMap_inter.hic\
+	/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_CMB_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_CMB_S_0.0.0_omegaMap_inter_withNorms.hic\
+	scripts/pullExpectedPixels.R 
+		mkdir -p data/h5
+		sbatch -J "pullExpectedPixels" -p general --mem=64G -t 1440 -n 1 -N 1 --wrap="R CMD BATCH scripts/pullExpectedPixels.R"
--- scripts/pullExpectedPixels.R
+++ scripts/pullExpectedPixels.R
@@ -1,6 +1,3 @@
-## Set i as 1:9
-i = 1
-
 ## Load required packages
 library(mariner)
 library(GenomeInfoDb)
@@ -11,25 +8,20 @@ load("data/raw/LIMA_binnedEPCounts.rda")
 ## LIMA .hic files are big so define
 ## the path to them here (backup hard-drive)
 ## Timepoint files
-timepoints <- 
-  list.files(path="/Volumes/LIMA_Processed_Data/hg19/hic/timepoints/",
-             pattern="inter.hic$",
-             recursive=TRUE,
-             full.names=TRUE)
-
+timepoints <- c("/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0000_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0000_S_0.0.0_megaMap_inter.hic",
+                "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0030_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0030_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0060_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0060_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0090_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0090_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0120_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0120_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0240_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0240_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_0360_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_0360_S_0.0.0_megaMap_inter.hic",
+		            "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_1440_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_1440_S_0.0.0_megaMap_inter.hic")
 ## Omega file
-omega <- 
-  list.files(path="/Volumes/LIMA_Processed_Data/hg19/hic/omega/",
-             pattern="inter_withNorms.hic$",
-             recursive=TRUE,
-             full.names=TRUE)
-
+omega <- "/proj/phanstiel_lab/Data/processed/LIMA/hic/LIMA_THP1_WT_LPIF_CMB_S_0.0.0_megaMap/aligned/LIMA_THP1_WT_LPIF_CMB_S_0.0.0_omegaMap_inter_withNorms.hic"
 hicFiles <- c(timepoints, omega)
-
 
 ## Reformat loops to remove 'chr' prefix
 seqlevelsStyle(epCounts) <- 'ENSEMBL'
-
 
 ## Remove chromosomes from epCounts
 ## that are not in the hicFiles...
@@ -39,21 +31,20 @@ keep <- chromsInLoops %in% chromsInHic
 chromsToKeep <- chromsInLoops[keep]
 seqlevels(epCounts) <- seqlevels(seqinfo(epCounts)[chromsToKeep])
 
+## Define hdf5 and rds filenames
+fn <- "data/h5/expectedCounts.h5"
+rn <- gsub(".h5$", ".rds", fn)
+
 system.time({
   res <-
-    pullHicPixels(x=epCounts[1:100000],
+    pullHicPixels(x=epCounts,
                   binSize=10e03,
-                  files=hicFiles[i],
+                  files=hicFiles,
+                  h5File=fn,
                   norm='KR',
                   matrix='expected',
-                  blockSize=1e06)
+                  blockSize=6e06)
 })
 
-## Save this path
-fp <- path(mariner::counts(res))
-fn <- file.path("data/h5", gsub(".hic$", ".h5", basename(hicFiles[i])))
-file.rename(from=fp, to=fn)
-path(SummarizedExperiment::assay(res)) <- fn
-
 ## Save this object
-saveRDS(object=res, file=gsub(".h5$", ".rds", fn))
+saveRDS(object=res, file=rn)
