082973be1d81a63bd2b61e97549c7c77d791b94b
EricSDavis <ericscottdavis@outlook.com>
2023-01-23 11:48:33 -0500

    Update path to count data manually.

--- scripts/accessExpectedCounts.R
+++ scripts/accessExpectedCounts.R
@@ -6,7 +6,9 @@ library(SummarizedExperiment)
 expectedCounts <- readRDS("data/h5/expectedCounts.rds")
 
 ## Update path to count data
-path(assay(expectedCounts)) <- "data/h5/expectedCounts.h5"
+## Not recommended, but the only way to update
+## paths once the object data has been broken
+expectedCounts@assays@data@listData$counts@seed@filepath <- "data/h5/expectedCounts.h5"
 
 ## Access count data
 assay(expectedCounts, "counts")
